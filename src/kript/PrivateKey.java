54f0f8320eb35a6fb45cbf22612aa1a1c8f1a82a
Austin VanAlstyne <ajv857@gmail.com>
2015-06-07 11:08:59 -0500

    Added licensing

--- src/kript/PrivateKey.java
+++ src/kript/PrivateKey.java
@@ -1,3 +1,28 @@
+/*
+* Kript
+* Created by Austin VanAlstyne
+* Open Source with Related GitHub Repo
+*
+* Copyright© 2015 Austin VanAlstyne
+*/
+
+/*
+*This file is part of Kript.
+*
+*Kript is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*Kript is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with Kript.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package kript;
 
 public class PrivateKey extends Key {
