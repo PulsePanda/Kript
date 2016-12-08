ebee7978270623251640fb834a836fd0dbbe8001
Austin VanAlstyne <ajv857@gmail.com>
2016-12-08 00:35:39 -0600

    testing adjustment to allow larger objects

--- src/kript/Kript.java
+++ src/kript/Kript.java
@@ -25,7 +25,10 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.SealedObject;
 
 /**
  * TO ENCODE, MUST SUBMIT A BYTE ARRAY OF THE MESSAGE
@@ -52,19 +55,14 @@ public class Kript {
 		try {
 			Kript k = new Kript();
 
-			final String originalText = "Text to be encrypted !@#$%^&*()_+";
-			final byte[] cipherText = k.encrypt(originalText.getBytes());
-			final byte[] decrypted = k.decrypt(cipherText);
+			String originalText = "Text to be encrypted !@#$%^&*()_+";
+			byte[] cipherText = k.encrypt(originalText.getBytes());
+			byte[] decrypted = k.decrypt(cipherText);
 
 			// Printing the Original, Encrypted and Decrypted Text
 			System.out.println("Original Text: " + originalText);
 
-			StringBuffer retString = new StringBuffer();
-			for (int i = 0; i < cipherText.length; ++i) {
-				retString.append(Integer.toHexString(0x0100 + (cipherText[i] & 0x00FF)).substring(1));
-			}
-
-			System.out.println("Encrypted Text: " + retString);
+			System.out.println("Encrypted Text: " + cipherText);
 
 			String retString1 = new String(decrypted);
 
@@ -91,7 +89,7 @@ public class Kript {
 
 		privateKey = key.getPrivate();
 
-		// remotePublicKey = key.getPublic();
+//		 remotePublicKey = key.getPublic();
 	}
 
 	/**
@@ -104,13 +102,9 @@ public class Kript {
 	 *             if there is an error with the encryption.
 	 */
 	public byte[] encrypt(byte[] bytes) throws Exception {
-		byte[] cipherText = null;
-		// get an RSA cipher object and print the provider
 		final Cipher cipher = Cipher.getInstance("RSA");
-		// encrypt the plain text using the public key
 		cipher.init(Cipher.ENCRYPT_MODE, remotePublicKey);
-		cipherText = cipher.doFinal(bytes);
-		return cipherText;
+		return blockCipher(bytes, cipher, Cipher.ENCRYPT_MODE);
 	}
 
 	/**
@@ -124,15 +118,97 @@ public class Kript {
 	 *             if there is an issue creating RSA cipher to decrypt.
 	 */
 	public byte[] decrypt(byte[] encryptedMessage) throws Exception {
-		byte[] decrypted = null;
-		// get an RSA cipher object and print the provider
 		final Cipher cipher = Cipher.getInstance("RSA");
-
-		// decrypt the text using the private key
 		cipher.init(Cipher.DECRYPT_MODE, privateKey);
-		decrypted = cipher.doFinal(encryptedMessage);
+		return blockCipher(encryptedMessage, cipher, Cipher.DECRYPT_MODE);
+	}
 
-		return decrypted;
+	/**
+	 * Handle the RSA side of things. Allows for items larger than the block
+	 * size to be encoded.
+	 * 
+	 * @param bytes
+	 *            byte[] array containing either plain or encoded data.
+	 * @param cipher
+	 *            Cipher object containing the mode, and related key.
+	 * @return byte[] with the appropriate encoded/plain data.
+	 */
+	private byte[] blockCipher(byte[] bytes, Cipher cipher, int mode) {
+		// string initialize 2 buffers.
+		// scrambled will hold intermediate results
+		byte[] scrambled = new byte[0];
+
+		// toReturn will hold the total result
+		byte[] toReturn = new byte[0];
+		// if we encrypt we use 100 byte long blocks. Decryption requires 128
+		// byte long blocks (because of RSA)
+		int length = (mode == Cipher.ENCRYPT_MODE) ? 100 : 128;
+
+		// another buffer. this one will hold the bytes that have to be modified
+		// in this step
+		byte[] buffer = new byte[length];
+
+		for (int i = 0; i < bytes.length; i++) {
+
+			// if we filled our buffer array we have our block ready for de- or
+			// encryption
+			if ((i > 0) && (i % length == 0)) {
+				// execute the operation
+				try {
+					scrambled = cipher.doFinal(buffer);
+				} catch (IllegalBlockSizeException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (BadPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				// add the result to our total result.
+				toReturn = append(toReturn, scrambled);
+				// here we calculate the length of the next buffer required
+				int newlength = length;
+
+				// if newlength would be longer than remaining bytes in the
+				// bytes array we shorten it.
+				if (i + length > bytes.length) {
+					newlength = bytes.length - i;
+				}
+				// clean the buffer array
+				buffer = new byte[newlength];
+			}
+			// copy byte into our buffer.
+			buffer[i % length] = bytes[i];
+		}
+
+		// this step is needed if we had a trailing buffer. should only happen
+		// when encrypting.
+		// example: we encrypt 110 bytes. 100 bytes per run means we "forgot"
+		// the last 10 bytes. they are in the buffer array
+		try {
+			scrambled = cipher.doFinal(buffer);
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// final step before we can return the modified data.
+		toReturn = append(toReturn, scrambled);
+
+		return toReturn;
+	}
+
+	private byte[] append(byte[] prefix, byte[] suffix) {
+		byte[] toReturn = new byte[prefix.length + suffix.length];
+		for (int i = 0; i < prefix.length; i++) {
+			toReturn[i] = prefix[i];
+		}
+		for (int i = 0; i < suffix.length; i++) {
+			toReturn[i + prefix.length] = suffix[i];
+		}
+		return toReturn;
 	}
 
 	/**
