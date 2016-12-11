f2488665f90c129fc496a7c5645b3a3a54930ca5
Austin VanAlstyne <ajv857@gmail.com>
2016-12-11 00:00:59 -0600

    Re-created project as gradle project
    worked just fine!

--- src/main/java/Kript/Kript.java
+++ src/main/java/Kript/Kript.java
@@ -17,7 +17,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package main.java.Kript;
+package Kript;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
